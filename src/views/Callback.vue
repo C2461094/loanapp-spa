a8beab0c88a9037f2c9a1366fecdb91dc7107494
Daniel Barboni <serpoiu@gmail.com>
2025-12-29 01:01:06 +0000

    fixed an error in the callback

--- src/views/Callback.vue
+++ src/views/Callback.vue
@@ -1,19 +1,22 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
 import { useRouter } from 'vue-router';
-import { getAuth0Client } from '@/composables/use-auth';
+import { initAuth0, isAuthenticated } from '@/composables/use-auth';
 
 const router = useRouter();
 
 onMounted(async () => {
-  const client = getAuth0Client();
-  await client.handleRedirectCallback(); 
-  router.replace('/'); 
+  await initAuth0();
+
+  // Optionally: redirect after successful login
+  if (isAuthenticated.value) {
+    router.push('/');
+  }
 });
 </script>
 
 <template>
   <div class="page">
-    <p>Logging in...</p>
+    <p>Logging you in...</p>
   </div>
 </template>
